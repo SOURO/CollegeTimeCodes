dfa784b98403471c53290bdde5c54e06dc7f0fb4
geekyStart <soura1990@gmail.com>
2016-07-14 11:15:47 +0530

    Adding Description and adding comment in code file

--- PROGRAM CODE/JAVA PROGRAM/hello_world/src/start_again.java
+++ PROGRAM CODE/JAVA PROGRAM/hello_world/src/start_again.java
@@ -5,6 +5,7 @@ class apples implements start_again_interface
 		System.out.println("Hello");
 	}
 }
+//main will be alaways void and static
 class start_again{
 	
 	public static void main(String args[])
